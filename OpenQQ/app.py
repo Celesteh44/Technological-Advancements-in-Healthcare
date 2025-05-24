779c59558cec64116bf1e95eec446686a1aa2b9a
Celesteh44 <herrerajessica17@gmail.com>
2025-05-24 13:22:03 -0500

    Update app.py

--- OpenQQ/app.py
+++ OpenQQ/app.py
@@ -279,4 +279,4 @@ def index():
     return render_template_string(HTML_TEMPLATE)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
